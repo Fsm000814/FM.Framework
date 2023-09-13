aac211b0fd0bf6dc47a24107ae9c740371002375
faceman <1002784867@qq.com>
2023-09-13 23:50:52 +0800

    tj

--- src/aspnet-core/src/FM.Framework.Web.Host/ConcurrencyException/AbpExServiceCollectionExtensions.cs
+++ src/aspnet-core/src/FM.Framework.Web.Host/ConcurrencyException/AbpExServiceCollectionExtensions.cs
@@ -6,30 +6,27 @@ using System;
 using FMFramework.AbpEX;
 using Abp.AspNetCore;
 
-namespace FMFranework.AbpEx
+namespace FMFramework.AbpEx
 {
     public static class FMFraneworkAbpExServiceCollectionExtensions
     {
-        //
-        // 摘要:
-        //     将 GCTAbpEx 集成到 Asp.Net Core
-        //
-        // 参数:
-        //   services:
-        //     服务注册器
-        //
-        //   optionsAction:
-        //     配置选项
-        //
-        //   removeConventionalInterceptors:
-        //     移除常规的拦截器
-        //
-        // 类型参数:
-        //   TStartupModule:
+        /// <summary>
+        /// 将 FMFraneworkAbpEx 集成到 Asp.Net Core
+        /// 封装了添加FMFrameworkAbpEx服务的逻辑，并在内部进行了一些自定义配置，
+        /// 例如添加Abp服务和执行AbpConfigureAspNetCoreAfter委托中的操作。
+        /// </summary>
+        /// <typeparam name="TStartupModule"></typeparam>
+        /// <param name="services">服务注册器</param>
+        /// <param name="optionsAction">配置选项</param>
+        /// <param name="removeConventionalInterceptors">移除常规的拦截器</param>
+        /// <returns>IServiceProvider实例</returns>
         public static IServiceProvider AddFMFrameworkAbpEx<TStartupModule>(this IServiceCollection services, Action<FMFrameworkAbpExBootstrapperOptions> optionsAction = null, bool removeConventionalInterceptors = true) where TStartupModule : AbpModule
         {
+            //对FMFrameworkAbpExBootstrapperOptions进行自定义配置
             FMFrameworkAbpExBootstrapperOptions fMFraneworkAbp = new FMFrameworkAbpExBootstrapperOptions();
+            //依赖注入fMFraneworkAbp
             optionsAction?.Invoke(fMFraneworkAbp);
+            //
             services.AddAbpWithoutCreatingServiceProvider<TStartupModule>(fMFraneworkAbp.AbpOptionsAction, removeConventionalInterceptors);
             AbpBootstrapper singletonServiceOrNull = services.GetSingletonServiceOrNull<AbpBootstrapper>();
             fMFraneworkAbp?.AbpConfigureAspNetCoreAfter?.Invoke(services);
--- src/aspnet-core/src/FM.Framework.Web.Host/Startup/Program.cs
+++ src/aspnet-core/src/FM.Framework.Web.Host/Startup/Program.cs
@@ -32,15 +32,21 @@ public class Program
         port = port == default ? 80 : port;
 
         return WebHost.CreateDefaultBuilder(args)
+             //配置Kestrel服务器，通过ConfigureKestrel方法设置了最大请求体大小为300MB。
              .ConfigureKestrel((context, options) =>
              {
                  options.Limits.MaxRequestBodySize = 1024 * 1024 * 300;
              })
+             //使用UseIIS和UseIISIntegration方法配置应用程序与IIS集成。
             .UseIIS()
             .UseIISIntegration()
+            //使用UseStartup方法指定启动类为Startup。
             .UseStartup<Startup>()
+            //使用UseKestrel方法监听指定端口号的任意IP地址
             .UseKestrel(op => op.ListenAnyIP(port))
+            //使用UseSerilog方法配置日志记录器
             .UseSerilog()
+            //调用Build方法构建WebHost实例
             .Build();
     }
     #region 日志配置
@@ -58,8 +64,7 @@ public class Program
         // 构建日志对象
         var cfg = new LoggerConfiguration()
             .ReadFrom
-            .Configuration(configuration)
-            ;
+            .Configuration(configuration);
 
         return cfg.CreateLogger();
     }

30b198a6996ea76fc0cb8ebbad203c30d4f7d55f
faceman <1002784867@qq.com>
2023-09-16 16:20:59 +0800

    ndo批量补充

--- aspnet-core/src/FM.FrameWork.Core/Ndo/GuidNdoDomainService/IGuidNdoDomainService.cs
+++ aspnet-core/src/FM.FrameWork.Core/Ndo/GuidNdoDomainService/IGuidNdoDomainService.cs
@@ -1,10 +1,19 @@
 ﻿using Abp.Dependency;
 using Abp.Domain.Entities;
 using Abp.Domain.Services;
+using Abp.Timing;
 
 using FM.FrameWork.Ndo.DomainService;
 
+using Microsoft.EntityFrameworkCore;
+
 using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+using System.Transactions;
+
+using static FM.FrameWork.FMFrameWorkConfigs;
 
 namespace FM.FrameWork.Ndo.GuidNdoDomainService
 {
@@ -15,5 +24,16 @@ namespace FM.FrameWork.Ndo.GuidNdoDomainService
         /// </summary>
         /// <returns></returns>
         Guid NewId();
+        public Task<List<TEntity>> BatchCreateNdo(List<TEntity> entities);
+        public Task<List<TEntity>> UnitOfWorkBatchCreateNdo(List<TEntity> entities, TransactionScopeOption opation = TransactionScopeOption.Required);
+        public Task<List<TEntity>> BatchCreateNotIdNdo(List<TEntity> entities);
+        public Task<List<TEntity>> UnitOfWorkBatchCreateNotIdNdo(List<TEntity> entities, TransactionScopeOption opation = TransactionScopeOption.Required);
+        public Task<List<TEntity>> BatchUpdateNdo(List<TEntity> entities);
+        public Task<List<TEntity>> UnitOfWorkBatchUpdateNdo(List<TEntity> entities, TransactionScopeOption opation = TransactionScopeOption.Required);
+        public Task<List<TEntity>> BatchCreateOrUpdateNdo(List<TEntity> entities);
+        public Task<List<TEntity>> BatchDeleteNdo(List<Guid> ids);
+        public Task DeleteByCondition(Expression<Func<TEntity, bool>> predicate);
+        public Task<List<TEntity>> BatchDeleteNdo(List<TEntity> entities);
+        public Task<List<TEntity>> UnitOfWorkBatchDeleteNdo(List<TEntity> entities, TransactionScopeOption opation = TransactionScopeOption.Required);
     }
 }
